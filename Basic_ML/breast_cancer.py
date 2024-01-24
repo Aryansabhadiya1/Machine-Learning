fd011fa5f1e2d500e6e526d0af70b9e0e5f1a3b3
Aryan <sabhadiyaaryan@gmail.com>
2024-01-24 14:57:19 +0530

    final commit

--- Basic_ML/breast_cancer.py
+++ Basic_ML/breast_cancer.py
@@ -3,34 +3,58 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 from sklearn.datasets import load_breast_cancer  # load dataset
 
-
-def model(X,y):
-
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-
-    lr_model = LogisticRegression()
-
-    # build dictionary of parameter to do hypertunnig
-    param_grid = {"max_iter":[1000,1500,1800]}
-
-    # find best model
-    grid_search = GridSearchCV(lr_model, param_grid, cv=5, scoring='accuracy')
-
-    # train model
-    grid_search.fit(X_train,y_train)
-
-    #predict value
-    y_pred = grid_search.predict(X_test)
-
-    #make report of perfomance
-    cr = classification_report(y_test,y_pred)
+# we build class to train our model
+class ML_trainer:
     
-    return cr
+    # # load dataset 
+    # def __init__(self,dataset):
+        
+    #     self.df = pd.read_csv(dataset)
     
-# load data in x and y
-X,y = load_breast_cancer(return_X_y=True)
+    # function for split_dataset
+    def split_data(self): # split_data(self,target_col)
+        
+        # self.X = df.drop(target_col,axis=1)
+        # self.y = df.drop[target_col]
+        self.X,self.y = load_breast_cancer(return_X_y=True)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y,test_size=0.2)
+        
+        self.model()
+    
+    # function for train model
+    def model(self):
 
-#Give data to train model X = independent values, y = target values
-cr = model(X,y)
+        lr_model = LogisticRegression()
 
-print(cr)
+        # build dictionary of parameter to do hypertunnig
+        param_grid = {"max_iter":[1000,1500,1800]}
+
+        # find best model
+        grid_search = GridSearchCV(lr_model, param_grid, cv=5, scoring='accuracy')
+
+        # train model
+        grid_search.fit(self.X_train,self.y_train)
+        
+        self.evaluation(grid_search)    
+    
+    # function for evaluate model
+    def evaluation(self,model):
+        
+        #predict value
+        self.y_pred = model.predict(self.X_test)
+
+        #make report of perfomance
+        cr = classification_report(self.y_test,self.y_pred)
+        
+        print(cr)
+    
+    
+if __name__ == "__main__":
+    
+    # Here we are create object of class for train our model
+    # Also we can give dataset path as argument for load dataset
+    trainer = ML_trainer() # ML_trainer(dataset_path)
+    
+    # In this method we will split training & testing data and after that all process is automatic
+    # We can also give our target_column here as argument to split but here i use inbuild sklearn data so I can't implement it.
+    trainer.split_data() # split_data("IsCancer")
--- Basic_ML/used_car_price_prediction.py
+++ Basic_ML/used_car_price_prediction.py
@@ -4,68 +4,95 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import LabelEncoder
 
-# function to train model parameter : dataset,target column
-def model(df,target_col):
+class ML_trainer:
+    
+    # load dataset 
+    def __init__(self,dataset_path):
         
-    # select all catagorical columns
-    categorical_columns = df.select_dtypes(include=['object']).columns
+        self.used_car_df = pd.read_csv(dataset_path,index_col=0)
+        
+    # function for split_dataset
+    def split_data(self,target_col):
+        
+        self.data_preprocessor()
+        self.feature_eng()
+        
+        self.X = self.used_car_df.drop(target_col,axis=1)
+        self.y = self.used_car_df.drop[target_col]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y,test_size=0.2)
+        
+        self.data_encoder()
+    
+    def data_encoder(self):
+        # select all catagorical columns
+        categorical_columns = self.used_car_df.select_dtypes(include=['object']).columns
 
-    label_encoder = LabelEncoder()
+        label_encoder = LabelEncoder()
 
-    for column in categorical_columns:
-        df[column] = label_encoder.fit_transform(df[column])
-    
-    # divide dataset into X and y    
-    X = df.drop(target_col,axis=1)
-    y = df[target_col]
-    
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+        for column in categorical_columns:
+            self.used_car_df[column] = label_encoder.fit_transform(self.used_car_df[column])
+            
+        self.model()
+            
+    # function to train model
+    def model(self):
 
-    linear_model = LinearRegression()
-    linear_model.fit(X_train,y_train)
-    
-    # make prediction
-    y_pred = linear_model.predict(X_test)
-    
-    mae = mean_absolute_error(y_test,y_pred)
-    
-    # return model score and error
-    return [linear_model.score(X_test,y_test),mae]
+        self.linear_model = LinearRegression()
+        self.linear_model.fit(self.X_train,self.y_train)
+        
+        self.evaluation()
+        
+    # function for model evaluation
+    def evaluation(self):
+        
+        self.y_pred = self.linear_model.predict(self.X_test)
+        
+        mae = mean_absolute_error(self.y_test,self.y_pred)
+        
+        print(f"Model score : {self.linear_model.score(self.X_test,self.y_test)}")
+        print(f"Model Error : {mae}")    
 
-def data_preprocessor(used_car_df):
+    # function for data preprocessing
+    def data_preprocessor(self):
+        
+        # remove unnecessary column
+        self.used_car_df.drop(["car_name","registration_year","manufacturing_year"],axis=1,inplace=True)
+        
+        # drop duplicates
+        self.used_car_df = used_car_df.drop_duplicates()
+        
+        self.feature_eng()
+        
+        #remove outliers
+        outlier_cols = ["mileage(kmpl)","engine(cc)","max_power(bhp)","torque(Nm)","price(in lakhs)"]
+        self.remove_outliers(outlier_cols)
     
-    # remove unnecessary column
-    used_car_df.drop(["car_name","registration_year","manufacturing_year"],axis=1,inplace=True)
-    # drop duplicates
-    used_car_df = used_car_df.drop_duplicates()
-    
-    #remove outliers
-    used_car_df = remove_outliers("mileage(kmpl)")
-    used_car_df = remove_outliers("engine(cc)")
-    used_car_df = remove_outliers("max_power(bhp)")
-    used_car_df = remove_outliers("torque(Nm)")
-    used_car_df = remove_outliers("price(in lakhs)")
-    
-    # create some useful feature
-    current_year = pd.to_datetime('today').year
-    used_car_df['manufacturing_year'] = pd.to_numeric(used_car_df['manufacturing_year'], errors='coerce').astype('Int64')
-    used_car_df['Car_Age'] = current_year - used_car_df['manufacturing_year']
-    used_car_df.dropna(inplace=True)
-    
-# fucntion for remove outlier    
-def remove_outliers(col):
-    q1 = used_car_df[col].quantile(0.25)
-    q3 = used_car_df[col].quantile(0.75)
-    IQR = q3-q1
-    
-    lower_limit = q1 - IQR*1.5
-    upper_limit = q3 + IQR*1.5
-    
-    return used_car_df.loc[(used_car_df[col]>lower_limit) & (used_car_df[col]<upper_limit)]
+    # function for create new features or to do feature selection    
+    def feature_eng(self):
+            
+        # create some useful feature
+        current_year = pd.to_datetime('today').year
+        self.used_car_df['manufacturing_year'] = pd.to_numeric(self.used_car_df['manufacturing_year'], errors='coerce').astype('Int64')
+        self.used_car_df['Car_Age'] = current_year - self.used_car_df['manufacturing_year']
+        self.used_car_df.dropna(inplace=True)
+        
+    # fucntion for remove outlier    
+    def remove_outliers(self,cols):
+        
+        for col in cols:
+            q1 = self.used_car_df[col].quantile(0.25)
+            q3 = self.used_car_df[col].quantile(0.75)
+            IQR = q3-q1
+            
+            lower_limit = q1 - IQR*1.5
+            upper_limit = q3 + IQR*1.5
+            
+            self.used_car_df =  self.used_car_df.loc[(self.used_car_df[col]>lower_limit) & (self.used_car_df[col]<upper_limit)]
 
-used_car_df = pd.read_csv("Used Car Dataset.csv",index_col=0)
-target_col = "price(in lakhs)"
-
-data_preprocessor(used_car_df)
-score,mae = model(used_car_df,target_col)
-print(score,mae)
+if __name__ == "__main__":
+    
+    dataset_path = "Used Car Dataset.csv"
+    target_col = "price(in lakhs)"
+    trainer = ML_trainer(dataset_path)
+    trainer.split_data(target_col)
+    
